6b164c94dc352d28da4abb2af3fdea7fe3cc698b
Lauren Champs <laurenkchamps@gmail.com>
2023-09-25 10:58:50 +0930

    Fix webpack errors

--- src/index.js
+++ src/index.js
@@ -2,12 +2,32 @@ import _ from 'lodash';
 import './style.css';
 import BackgroundImg from './pizza.jpg';
 
+console.log('Hello World');
+
 function component() {
-    const element = document.createElement('div');
+    const header = document.createElement('header');
 
-    element.innerHTML = _.join(['Hello', 'World'], ' ');
-
-    return element;
+    return header;
 }
 
-document.body.appendChild(component());
+document.getElementById('content').appendChild(component());
+
+// function component() {
+//     const header = document.createElement('header');
+//     const nav = document.createElement('nav');
+//     const menu = document.createElement('ul');
+
+//     const menuItems = ['Home', 'Menu', 'Contact'];
+
+//     for(let i = 0; i < menuItems.length; i++) {
+//         const li = document.createElement('li');
+//         li.appendChild(document.createTextNode(menuItems[i]));
+//         menu.appendChild(menuItem);
+//     }
+
+//     nav.appendChild(menu);
+//     header.appendChild(nav);
+
+//     return header;
+// }
+
